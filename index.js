074ceee1765be1377618984a32fd8045880b796b
Palash Chitnavis <palashchitnavis@gmail.com>
2023-10-08 12:12:42 +0530

    commit-1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let menu = document.querySelector('#menu-bars')
 let navbar = document.querySelector('.navbar')
-
+let navopen = document.querySelector('.navbar.active')
 menu.addEventListener('click' , () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
